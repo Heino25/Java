31db5b64174d154442c216afda8444fd0bd1ebca
Heino <heinoduplessis@gmail.com>
2021-10-10 19:26:21 +0200

    10/10/21

--- Java OCP1 ClassWork/Projects/JavaMasterClass/FloatAndDouble/src/com/company/Main.java
+++ Java OCP1 ClassWork/Projects/JavaMasterClass/FloatAndDouble/src/com/company/Main.java
@@ -3,6 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        float myMinfloatValue = Float.MIN_VALUE;
+        float myMaxfloatValue = Float.MAX_VALUE;
+        System.out.println("Float Minimum Value = " + myMinfloatValue);
+        System.out.println("Float Maximum Value = " + myMaxfloatValue);
+
+        double myMindoubleValue = Double.MIN_VALUE;
+        double myMaxdoubleValue = Double.MAX_VALUE;
+        System.out.println("Double Max value = " + (myMaxdoubleValue + 1));
+        System.out.println("Double Min value = " + (myMindoubleValue - 1));
+
+        int myIntValue = 5;
+        float myFloatValue = (float) 5.25;
+        double myDoubleValue = 5.25;
+        System.out.println("MyIntValue= " + myIntValue);
+        System.out.println("MyFloatValue= " + myFloatValue);
+        System.out.println("MyIntValue= " + myDoubleValue);
     }
 }
