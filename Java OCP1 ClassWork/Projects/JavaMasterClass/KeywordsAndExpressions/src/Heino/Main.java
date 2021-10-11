2d0e8f274e1bf7d66141eec16dee26a2cce33ac5
Heino <heinoduplessis@gmail.com>
2021-10-11 08:11:14 +0200

    11/10/21

---
+++ Java OCP1 ClassWork/Projects/JavaMasterClass/KeywordsAndExpressions/src/Heino/Main.java
@@ -0,0 +1,21 @@
+package Heino;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+
+        if(highScore == 50) {
+            System.out.println("This is am expression");
+        }
+
+        // What is the expressions in the following code.
+        int score = 100; //score = 100 expressions
+        if (score > 99) { //score > 99 expressions
+            System.out.println("You got the High score!"); //"You got the High score!" expressions
+            score = 0; //score = 0 expressions
+        }
+    }
+}
