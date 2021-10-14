51145a2d48aa6a9f8d7b2049675f75affd5e1148
Heino <heinoduplessis@gmail.com>
2021-10-14 07:51:19 +0200

    14/10/21

---
+++ Java OCP1 ClassWork/Projects/JavaMasterClass/Constructors/src/Heino/Main.java
@@ -0,0 +1,17 @@
+package Heino;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Account bobsAccount = new Account(); //("12345", 0.00, "Bob Brown", "myemail.com", "(078) 123-4567");
+        System.out.println(bobsAccount.getNumber());
+        bobsAccount.withdrawal(100.0);
+
+        bobsAccount.deposit(50.0);
+        bobsAccount.withdrawal(100.0);
+
+        bobsAccount.deposit(51.0);
+        bobsAccount.withdrawal(100.0);
+    }
+}
