1887785f43b0ceea05ceb4b028751fc18d2cb087
Heino <heinoduplessis@gmail.com>
2021-10-16 13:17:10 +0200

    16/10/21

--- Java OCP1 ClassWork/Projects/JavaMasterClass/OOPTest/src/Heino/Main.java
+++ Java OCP1 ClassWork/Projects/JavaMasterClass/OOPTest/src/Heino/Main.java
@@ -10,5 +10,10 @@ public class Main {
         hamburger.addHamburgerAddition2("Lettuce", 0.75);
         hamburger.addHamburgerAddition3("Cheese", 1.13);
         System.out.println("Total Burger price is $" + hamburger.itemizeHamburger());
+
+        HealthyBurger healthyBurger = new HealthyBurger("Bacon", 5.67);
+        healthyBurger.addHamburgerAddition1("Egg", 5.43);
+        healthyBurger.addHealthyAddition1("Lentils", 3.41);
+        System.out.println("Total Healthy Burger price is " + healthyBurger.itemizeHamburger());
     }
 }
