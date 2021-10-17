ebe8c172dcece8c257e982bb52b083409cc17d01
Heino <heinoduplessis@gmail.com>
2021-10-17 18:26:40 +0200

    17/10/21

---
+++ Java OCP1 ClassWork/Projects/JavaMasterClass/LinkedListChallenge/src/Heino/Song.java
@@ -0,0 +1,19 @@
+package Heino;
+public class Song {
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return this.title + ": " + this.duration;
+    }
+}
