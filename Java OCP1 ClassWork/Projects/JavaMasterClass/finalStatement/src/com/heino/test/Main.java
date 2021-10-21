68f2e219c243e871284d32f6a88f08ead56271eb
Heino <heinoduplessis@gmail.com>
2021-10-21 14:49:35 +0200

    21/10/21

---
+++ Java OCP1 ClassWork/Projects/JavaMasterClass/finalStatement/src/com/heino/test/Main.java
@@ -0,0 +1,21 @@
+package com.heino.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    SomeClass one = new SomeClass("one");
+        SomeClass two = new SomeClass("two");
+        SomeClass three = new SomeClass("three");
+
+        System.out.println(one.getInstanceNumber());
+        System.out.println(two.getInstanceNumber());
+        System.out.println(three.getInstanceNumber());
+
+
+
+//        System.out.println("Main method called");
+//        SIBTest test = new SIBTest();
+//        test.someMethod();
+//        System.out.println("Owner is " + SIBTest.owner);
+    }
+}
