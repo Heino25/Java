ea8da0846bfb054ac3849a69b255238b1863de83
Heino <heinoduplessis@gmail.com>
2021-11-05 15:18:24 +0200

    05/11/21

--- Java HerbSchildt/HerbSchildt/src/main/java/Chapter4/MyClass.java
+++ Java HerbSchildt/HerbSchildt/src/main/java/Chapter4/MyClass.java
@@ -11,19 +11,19 @@ package Chapter4;
  */
 // A simple constructor. 
   
-class MyClass { 
-  int x; 
- 
-  MyClass() { 
-    x = 10; 
-  }   
-}   
-   
-class ConsDemo {   
-  public static void main(String args[]) {   
-    MyClass t1 = new MyClass(); 
-    MyClass t2 = new MyClass(); 
- 
-    System.out.println(t1.x + " " + t2.x); 
-  }   
-}
+//class MyClass { 
+//  int x; 
+// 
+//  MyClass() { 
+//    x = 10; 
+//  }   
+//}   
+//   
+//class ConsDemo {   
+//  public static void main(String args[]) {   
+//    MyClass t1 = new MyClass(); 
+//    MyClass t2 = new MyClass(); 
+// 
+//    System.out.println(t1.x + " " + t2.x); 
+//  }   
+//}
--- Java HerbSchildt/HerbSchildt/src/main/java/Chapter4/Vehicle.java
+++ Java HerbSchildt/HerbSchildt/src/main/java/Chapter4/Vehicle.java
@@ -16,18 +16,18 @@ package Chapter4;
    fuel required for a given distince. 
 */ 
  
-class Vehicle {  
-  int passengers; // number of passengers  
-  int fuelcap;    // fuel capacity in gallons 
-  int mpg;        // fuel consumption in miles per gallon 
- 
-  // Return the range. 
-  int range() { 
-    return mpg * fuelcap; 
-  } 
- 
-  // Compute fuel needed for a given distance. 
-  double fuelneeded(int miles) { 
-    return (double) miles / mpg; 
-  } 
-}  
+//class Vehicle {  
+//  int passengers; // number of passengers  
+//  int fuelcap;    // fuel capacity in gallons 
+//  int mpg;        // fuel consumption in miles per gallon 
+// 
+//  // Return the range. 
+//  int range() { 
+//    return mpg * fuelcap; 
+//  } 
+// 
+//  // Compute fuel needed for a given distance. 
+//  double fuelneeded(int miles) { 
+//    return (double) miles / mpg; 
+//  } 
+//}  
